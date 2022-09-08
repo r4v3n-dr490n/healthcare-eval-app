8c32ab05c813694cab9d2616938e83d7af5fb086
Mohamed A. Atti <112394411+r4v3n-dr490n@users.noreply.github.com>
2022-09-08 13:52:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ st.set_page_config(page_title="Healthcare Quality Eval App",
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+	footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
